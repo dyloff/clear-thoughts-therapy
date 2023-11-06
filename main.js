23ff7243e45121f0ca8d3b14f9d86e1b014f797e
Dylon <dylonmura@gmail.com>
2023-11-06 13:09:23 +0000

    scroll animation + homepage animation

---
+++ main.js
@@ -0,0 +1,26 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__container h2", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__container img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
